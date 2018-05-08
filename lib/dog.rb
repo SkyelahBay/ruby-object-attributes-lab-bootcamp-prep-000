010fb0044a032616ce282dfb5f64e2849069c8a4
Skyelah Hanna Bay <skyelahbay@gmail.com>
2018-05-08 20:25:47 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,17 @@
 class Dog
-  def initialize(name)
+  def initialize(name, breed)
     @name = name
+    @breed = breed
   end
   
-  def name=(dog_name)
-    @name = dog_name
+  def identity_set=(dog_name_and_breed)
+    name, breed = dog_name_and_breed.split
+    @name = name
+    @breed = breed
   end
   
-  def name
-    @name
+  def identity
+    "#{@name} #{@breed}"
   end
-  
-   
   
 end
